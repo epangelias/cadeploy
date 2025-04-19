adc84a2a0239c27cb831d3b65a77a652066c97fa
Epangelias <epangelias@gmail.com>
2025-04-19 21:08:54 +0700

    fix

--- lib/caddy.ts
+++ lib/caddy.ts
@@ -36,7 +36,7 @@ export async function reverseProxy(host: string, port: number) {
               }
             ]
           }
-        ]:
+        ]
       },
     ],
     match: [{ host: [host] }],
